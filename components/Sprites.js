2c7797a9eb40b227a3cb3930f4f345025672f05e
Nate <natediven1@gmail.com>
2021-11-13 03:16:12 -0700

    Update Sprites.js

--- components/Sprites.js
+++ components/Sprites.js
@@ -1,30 +1,27 @@
 import Link from 'next/link';
+import styles from '../styles/Sprites.module.css';
 
 export default function Sprites({ source }) {
-	return <div></div>;
+	return (
+		<div className={styles.main}>
+			{source.map((value, index) => {
+				return (
+					<Link key={index} href={value}>
+						<a>
+							<img src={value} width="100px" height="100px" />
+						</a>
+					</Link>
+				);
+			})}
+
+			<style jsx>{`
+				.main {
+					display: grid;
+					grid-template-columns: 1fr 1fr 1fr 1fr;
+					width: 100%;
+					place-items: center;
+				}
+			`}</style>
+		</div>
+	);
 }
-
-// export default function Sprites({ source }) {
-// 	return (
-// 		<div className="main">
-// 			{source.map((value, index) => {
-// 				return (
-// 					<Link key={index} href={value}>
-// 						<a>
-// 							<img src={value} width="100px" height="100px" />
-// 						</a>
-// 					</Link>
-// 				);
-// 			})}
-
-// 			<style jsx>{`
-// 				.main {
-// 					display: grid;
-// 					grid-template-columns: 1fr 1fr 1fr 1fr;
-// 					width: 100%;
-// 					place-items: center;
-// 				}
-// 			`}</style>
-// 		</div>
-// 	);
-// }
