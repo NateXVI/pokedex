d9839b7fc0e9b9b6a9dd32b60c56546d3faf2d32
Nate <natediven1@gmail.com>
2021-11-13 03:12:50 -0700

    Update Sprites.js

--- components/Sprites.js
+++ components/Sprites.js
@@ -1,27 +1,30 @@
 import Link from 'next/link';
-import Image from 'next/Image';
 
 export default function Sprites({ source }) {
-	return (
-		<div className="main">
-			{source.map((value, index) => {
-				return (
-					<Link key={index} href={value}>
-						<a>
-							<img src={value} width="100px" height="100px" />
-						</a>
-					</Link>
-				);
-			})}
-
-			<style jsx>{`
-				.main {
-					display: grid;
-					grid-template-columns: 1fr 1fr 1fr 1fr;
-					width: 100%;
-					place-items: center;
-				}
-			`}</style>
-		</div>
-	);
+	return <div></div>;
 }
+
+// export default function Sprites({ source }) {
+// 	return (
+// 		<div className="main">
+// 			{source.map((value, index) => {
+// 				return (
+// 					<Link key={index} href={value}>
+// 						<a>
+// 							<img src={value} width="100px" height="100px" />
+// 						</a>
+// 					</Link>
+// 				);
+// 			})}
+
+// 			<style jsx>{`
+// 				.main {
+// 					display: grid;
+// 					grid-template-columns: 1fr 1fr 1fr 1fr;
+// 					width: 100%;
+// 					place-items: center;
+// 				}
+// 			`}</style>
+// 		</div>
+// 	);
+// }
