7d56f80933c99f7e73e3f7368bedabe2376d0c96
Nate <natediven1@gmail.com>
2021-11-14 05:37:03 -0700

    Update Types.js

--- components/Types.js
+++ components/Types.js
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import styles from '../styles/Types.module.css';
 
 export default function Types({ types }) {
-	console.log(types);
 	types = types.map((v) => {
 		return {
 			name: v.type.name,
