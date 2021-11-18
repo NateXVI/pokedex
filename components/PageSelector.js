685d87d1866612d9583d3611ed836755984865c1
Nate Diven <natediven1@gmail.com>
2021-11-17 18:37:09 -0700

    Changed page navigation
    -Changed PageSelector to Page Navigator
    -Added new Page Selector function that lets you go to a certain page

--- components/PageSelector.js
+++ components/PageSelector.js
@@ -1,35 +1,57 @@
 import Link from 'next/link';
 import styles from '../styles/PageSelector.module.css';
+import router from 'next/router';
+import { useState } from 'react';
 
-// export default function PageSelector({ path, currentPage, maxPage }) {
-// 	return (
-// 		<div className="m-1 mb-3">
-// 			<p className={`${styles.text} `}>
-// 				{currentPage > 1 ? (
-// 					<Link href={{ pathname: path, query: { page: currentPage - 1 } }}>
-// 						<a className={styles.button}>{`${currentPage - 1} Back`}</a>
-// 					</Link>
-// 				) : (
-// 					<></>
-// 				)}
-// 				<span className={styles.text}>{currentPage}</span>
-// 				{currentPage < maxPage ? (
-// 					<Link href={{ pathname: path, query: { page: Number(currentPage) + 1 } }}>
-// 						<a className={styles.button}>{`Next ${Number(currentPage) + 1}`}</a>
-// 					</Link>
-// 				) : (
-// 					<></>
-// 				)}
-// 			</p>
-// 		</div>
-// 	);
-// }
-export default function PageSelector({ path, currentPage, maxPage }) {
+export default function PageSelector({ path, maxPage, currentPage }) {
+	const [pageInput, setPageInput] = useState(currentPage);
+
+	function goTo(event) {
+		console.log(pageInput);
+
+		if (pageInput != currentPage) {
+			router.push(`${path}?page=${pageInput}`);
+		}
+	}
+
+	function setInput(value) {
+		value = Math.min(value, maxPage);
+		value = Math.max(1, value);
+
+		setPageInput(value);
+	}
+
+	return (
+		<div className="mx-1 flex-none">
+			<input
+				type="number"
+				min="1"
+				max={maxPage}
+				value={pageInput}
+				onChange={(e) => setInput(e.target.value)}
+				className="h-10 px-2 sm:px-4 rounded-l-md"
+			/>
+			<button
+				className="bg-blue-600 text-white h-10 w-16 rounded-r-md"
+				onClick={goTo}
+			>
+				Go To
+			</button>
+		</div>
+	);
+}
+
+export function PageNavigator({ path, currentPage, maxPage }) {
 	return (
 		<div className="w-full">
-			<div className="container w-full flex items-center justify-center m-auto sm:m-0">
+			<div className="flex m-auto sm:m-0">
 				{currentPage > 1 ? (
-					<Link href={{ pathname: path, query: { page: currentPage - 1 } }}>
+					<Link
+						href={{
+							pathname: path,
+							query: { page: currentPage - 1 },
+						}}
+					>
 						<a className={styles.button}>{`Back`}</a>
 					</Link>
 				) : (
@@ -41,7 +63,12 @@ export default function PageSelector({ path, currentPage, maxPage }) {
 				</p>
 
 				{currentPage < maxPage ? (
-					<Link href={{ pathname: path, query: { page: Number(currentPage) + 1 } }}>
+					<Link
+						href={{
+							pathname: path,
+							query: { page: Number(currentPage) + 1 },
+						}}
+					>
 						<a className={styles.button}>{`Next`}</a>
 					</Link>
 				) : (
