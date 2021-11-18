872b437e7473eda08d4fe9b0a19d000117787349
Nate <natediven1@gmail.com>
2021-11-17 21:37:32 -0700

    Update GameCovers.js

--- components/GameCovers.js
+++ components/GameCovers.js
@@ -2,14 +2,14 @@ import Image from 'next/image';
 
 export default function GameCovers({ games }) {
 	return (
-		<div className="grid grid-cols-4">
+		<div className="grid grid-cols-4 gap-1 sm:gap-2 place-items-center">
 			{games.map((game, index) => {
 				return (
-					<div key={index} className="m-2 transition-shadow">
+					<div key={index} className="">
 						<Image
 							key={index}
-							width="200"
-							height="200"
+							width="128"
+							height="128"
 							src={`/covers/${game}.jpg`}
 						></Image>
 					</div>
